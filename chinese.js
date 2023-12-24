3203bc8bc5ec9fdbfb3def5bfc4d3f73bba5464d
Anluongk25 <luongmyan1502@gmail.com>
2023-12-24 11:57:55 +0700

    Update Final Project

---
+++ chinese.js
@@ -0,0 +1,49 @@
+fetch("./chinese.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (result) {
+    console.log(result);
+    result.forEach(function (item, index) {
+	
+      const card = document.getElementById("Chinese");
+      const imgTag = document.createElement("img");
+
+      const divTag2 = document.createElement("div");
+      const divTag3 = document.createElement("div");
+      const divTag4 = document.createElement("div");
+      const name = document.createElement("h5");
+      const description = document.createElement("p");
+      const materials = document.createElement("p");
+      imgTag.classList.add("card-img-top");
+      card.classList.add("row");
+      card.classList.add("row-cols-1");
+      card.classList.add("row-cols-md-3");
+      card.classList.add("g-4");
+      card.classList.add("card-group");
+      divTag2.classList.add("col");
+      divTag3.classList.add("card");
+      divTag3.classList.add("h-100");
+
+      divTag4.classList.add("card-body");
+      name.classList.add("card-title");
+      description.classList.add("card-text");
+      materials.classList.add("card-text");
+      imgTag.src = item.imgUrl;
+      name.innerText = item.name;
+      description.innerText = item.description;
+      materials.innerText = item.materials;
+      divTag4.appendChild(name);
+      divTag4.appendChild(description);
+      divTag4.appendChild(materials);
+      divTag3.appendChild(imgTag);
+      divTag3.appendChild(divTag4);
+      divTag2.appendChild(divTag3);
+      card.appendChild(divTag2);
+	  
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
---
+++ content2.js
@@ -0,0 +1,53 @@
+fetch("./content2.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (result) {
+    console.log(result);
+    result.forEach(function (item, index) {
+      //     <div id="content" class="card mb-3">
+      //     <img src="..." class="card-img-top" alt="...">
+      //     <div class="card-body">
+      //       <h5 class="card-title">Card title</h5>
+      //       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      //       <p class="card-text"></p>
+      //       <p class="card-text"></p>
+
+      //     </div>
+      //   </div>
+      const content = document.getElementById("content");
+      const imgTag = document.createElement("img");
+
+      const divTag = document.createElement("div");
+      const content1 = document.createElement("p");
+      const content2 = document.createElement("p");
+      const content3 = document.createElement("p");
+      const name = document.createElement("h5");
+      content.classList.add("card");
+      content.classList.add("mb-3");
+      
+
+      imgTag.classList.add("card-img-top");
+      divTag.classList.add("card-body");
+      
+      content1.classList.add("card-text");
+      content2.classList.add("card-text");
+      content3.classList.add("card-text");
+      name.classList.add("card-title");
+      imgTag.src = item.imgUrl;
+      name.innerText = item.name;
+
+      content1.innerText = item.content1;
+      content2.innerText = item.content2;
+      content3.innerText = item.content3;
+      divTag.appendChild(name);
+      divTag.appendChild(content1);
+      divTag.appendChild(content2);
+      divTag.appendChild(content3);
+      content.appendChild(divTag);
+      content.appendChild(imgTag);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
